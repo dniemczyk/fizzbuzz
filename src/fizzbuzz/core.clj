01a586a969e887f9f0d08d6dea3aeac00ef13312
Damian Niemczyk <djniemczyk7@googlemail.com>
2015-10-14 07:54:46 +0200

    Initial commit

---
+++ src/fizzbuzz/core.clj
@@ -0,0 +1,14 @@
+(ns fizzbuzz.core
+  (:gen-class))
+
+(defn fizz
+  "Changes an integer to the word 'Fizz' if it is divisible through 3"
+  [x]
+  (if (= 0 (mod x 3))
+    "Fizz"
+    x))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
