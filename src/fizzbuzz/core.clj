27c8ff2a9cc99ccde23485a160a7e7cc744d48d5
Damian Niemczyk <djniemczyk7@googlemail.com>
2015-10-23 23:32:07 +0200

    Rewrite fizz and buzz to use the macro

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -6,7 +6,7 @@
   "Changes the integer to string s if the integer is divisible through
    modulo. Basic building block for the Fizz and Buzz function from
    the fizzbuzz problem"
-  [modulo s]
+  [s modulo]
   `(fn
      ([x#]
       (if (zero? (mod x# ~modulo)) ~s x#))
@@ -17,31 +17,9 @@
           ~s)
         y#))))
 
-(defn fizz
-  "Changes an integer to the word 'Fizz' if it is divisible through 3"
-  ([x]
-   (if (zero? (mod x 3))
-     "Fizz"
-     x))
-  ([x y]
-   (if (zero? (mod x 3))
-     (if (string? y)
-       (str y "Fizz")
-       "Fizz")
-     y)))
+(def fizz (string-if-modulo "Fizz" 3))
 
-(defn buzz
-  "Changes an integer to the word 'Buzz' if it is divisible through 5"
-  ([x]
-   (if (zero? (mod x 5))
-     "Buzz"
-     x))
-  ([x y]
-   (if (zero? (mod x 5))
-     (if (string? y)
-       (str y "Buzz")
-       "Buzz")
-     y)))
+(def buzz (string-if-modulo "Buzz" 5))
 
 (defn fizzbuzz [x]
   (let [xs (range 1 x)]
